7b83d22fbae187bdd2342f466ba74ba2cf3769f4
PauloHenriqque <hhhenriquepaulo@gmail.com>
2025-11-26 14:47:44 -0300

    feat: criando __init__

---
+++ processors/__init__.py
@@ -0,0 +1,13 @@
+from .stats_processor import GameStatsProcessor
+from .report_generator import ReportGenerator
+from .achievement_processor import AchievementProcessor
+from .action_logger import ActionLogger
+from .performance_monitor import PerformanceMonitor
+
+__all__ = [
+    'GameStatsProcessor',
+    'ReportGenerator',
+    'AchievementProcessor',
+    'ActionLogger',
+    'PerformanceMonitor'
+]
